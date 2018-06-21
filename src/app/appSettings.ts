f481ea57549067ad11017fc7ed538599cd1a803f
cscummings <cs.cummings@gmail.com>
2018-06-21 05:08:34 -0700

    update demo db and appSettings

--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -5,7 +5,7 @@ export class AppSettings {
   // public static BATCH_SERVICE_API = 'http://localhost:8096/PathometerApplication/api/pathos_batch/';
   // public static BATCH_SERVICE_TOTALQUEUE_COUNT = 'http://localhost:8096/PathometerApplication/api/pathos_batch/totalQueueCount/' ;
   // Json Server prototype api
-  public static BASE= 'http://localhost:3000';
+  public static BASE = 'http://localhost:3000';
   public static BATCH_SERVICE_API_BACKLOG = 'http://localhost:3000/backlog';
   public static BATCH_SERVICE_API_CURRENT_COUNT = 'http://localhost:3000/count';
   public static BATCH_SERVICE_API = 'http://localhost:3000/batches';
